7c58d582c9c8a465aae6217436b6cc5c3885ef0d
= <=>
2023-03-13 15:00:13 +0800

    use module orca

--- Practice/Week3/Exercise3/relativistic_correction.sh
+++ Practice/Week3/Exercise3/relativistic_correction.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$PATH
-export LD_LIBRARY_PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$LD_LIBRARY_PATH
+module load orca/4.2
+#export PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$PATH
+#export LD_LIBRARY_PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$LD_LIBRARY_PATH
 
 for i in $(ls *inp | sort -n);
 do
-  /home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411/orca $i > ${i%.???}.out
+  orca $i > ${i%.???}.out
 done
 
