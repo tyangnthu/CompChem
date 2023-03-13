d39012ab5827281e5b8246627a3b23cba887d7f7
= <=>
2023-03-13 14:27:05 +0800

    debugged w3e3

--- Practice/Week3/Exercise3/relativistic_correction.sh
+++ Practice/Week3/Exercise3/relativistic_correction.sh
@@ -3,9 +3,8 @@
 export PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$PATH
 export LD_LIBRARY_PATH=/home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411:$LD_LIBRARY_PATH
 
-for i in *inp;
+for i in $(ls *inp | sort -n);
 do
   /home/u4/nickyang69/software/orca_5_0_2_linux_x86-64_shared_openmpi411/orca $i > ${i%.???}.out
 done
 
-#for i in $(ls *R-nr* | sort -n); do grep 'FINAL SINGLE' $i | awk '{print $5}'; done
