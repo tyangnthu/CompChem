88fd60f1cd02f4606b2fe5c2dc1a1fa3fcf680a8
tyangnthu <tyang@mx.nthu.edu.tw>
2023-05-07 21:17:04 +0800

    added w10e

---
+++ Practice/Week10/Exercise2/Outputs/activate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# setting environments
+module load cuda/11.7
+module load openmpi4/4.1.4
+export PATH=$HOME/software/gromacs/2022.2/bin:$PATH
+#module load gcc/11.2.0
+#alias gmx=/home/nickyang69/software/gromacs/2022.2/bin/gmx_mpi
